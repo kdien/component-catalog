2d52a72356703ddafe48fab3b9daa6d9562ebe5d
Khoa Dien <khoa.dien@hotmail.com>
2019-04-10 16:12:39 -0300

    Add language Create view and modify controller code accordingly

--- src/main/java/com/oultoncollege/ComponentCatalog/controllers/LanguageController.java
+++ src/main/java/com/oultoncollege/ComponentCatalog/controllers/LanguageController.java
@@ -2,35 +2,39 @@ package com.oultoncollege.ComponentCatalog.controllers;
 
 import com.oultoncollege.ComponentCatalog.models.Language;
 import com.oultoncollege.ComponentCatalog.repositories.LanguageRepository;
+import com.oultoncollege.ComponentCatalog.services.LanguageServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
-@RequestMapping(path = "/languages")
+@RequestMapping("/language/*")
 public class LanguageController {
 
     @Autowired
     private LanguageRepository languageRepository;
 
-    @GetMapping(path = "/add")
-    public @ResponseBody
-    String addNewLanguage
-        (@RequestParam String name) {
+    @GetMapping(path = "/create")
+    public String createLanguageForm(Model model) {
 
-        Language l = new Language();
-        l.setName(name);
-        languageRepository.save(l);
-        return "Successful";
+
+        model.addAttribute("language", new Language());
+        return "language/create";
     }
 
-    @GetMapping(path = "/all")
-    public @ResponseBody
-    Iterable<Language> getAllLanguages() {
-        return languageRepository.findAll();
-    }
+    @PostMapping(path = "/create")
+    public String createLanguageSubmit(@ModelAttribute Language language, BindingResult result, Model model) {
+        LanguageServiceImpl languageService = new LanguageServiceImpl(languageRepository);
+        languageService.createLanguage(language);
 
+        if (result.hasErrors()) {
+            model.addAttribute("success", false);
+        } else {
+            model.addAttribute("success", true);
+        }
+
+        return "redirect:/language/create";
+    }
 }
